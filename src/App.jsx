d855558d0804dc5aaf480abfb6cceabd5983f15a
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-04-24 09:37:25 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Informacion from './components/Informacion';
+
+function App() {
+  
+
+  return (
+    <main className=' container my-5'>
+      <Informacion></Informacion>
+    </main>
+    
+  )
+}
+
+export default App
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,12 @@
+
+
+const Informacion = ({mensaje}) => {
+    return (
+    <section>
+      <h1>Hello {mensaje}</h1>
+    </section>
+    );
+};
+
+export default Informacion;
+
